12c6c3a737e70f3f5fd23c7df03fdc924ac5a3b4
Asad Khan <auk5592@gmail.com>
2017-11-19 23:01:56 +0100

    completed app

--- controllers/OrderService.js
+++ controllers/OrderService.js
@@ -1,6 +1,6 @@
 'use strict';
 var low	= require('lowdb');
-const db = low('orderFile.json');
+const db = low('pizzaFile.json');
 
 //creating a random 3 digit number for ID
 function uuid() {
@@ -11,14 +11,44 @@ exports.createOrder = function(args, res, next) {
   /**
    * Place an order.
    **/
-
+  var unikId = uuid();
   res.setHeader('Content-Type', 'application/json');
   res.statusCode = 201;
-
+  
   var result = db.get('orders')
-  .push({ id: uuid(), orderItems: args.body.value.orderItems, totalPrice: args.body.value.totalPrice, recipient: args.body.value.recipient })
+  .push({ id: unikId, orderItems: args.body.value.orderItems, totalPrice: args.body.value.totalPrice, recipient: args.body.value.recipient })
   .value()
-res.end(JSON.stringify(result));
+  var result1 = db.get('orders')
+  .find({id: unikId})
+  .assign({ totalPrice: calculateTotal(unikId)})
+  .value()
+  
+  res.end(JSON.stringify(result1));
+  
+}
+
+function calculateTotal (orderid){
+  var orderID = orderid;
+  var pizzaID = JSON.stringify(db.get('orders').filter({id: orderID}).map((order) => { return order.orderItems[0].pizzaId; }).value()[0]);
+  console.log(pizzaID);
+  
+  var pizzaPrice = JSON.stringify(db.get('pizzas').filter({id: parseInt(pizzaID)}).map((pizza) => { return pizza.price; }).value()[0]);
+  console.log(parseFloat(pizzaPrice));
+
+  var toppingPrice = JSON.stringify(db.get('toppings').filter({pizzaId: parseInt(pizzaID)}).map((topping) => { return topping.price; }));
+  
+  toppingPrice = JSON.parse(toppingPrice);
+  const total = toppingPrice.reduce(function(sum, value) {
+    return sum + value;
+  });
+
+  console.log(total);
+  
+  var orderTotal = parseFloat(pizzaPrice) + parseFloat(total);
+  console.log("The total for this order is : " + orderTotal);
+  console.log("order id: " + orderID);
+
+  return parseFloat(orderTotal);
 }
 
 exports.deleteOrder = function(args, res, next) {
@@ -57,6 +87,7 @@ exports.getOrderById = function(args, res, next) {
  res.end(JSON.stringify(db.get('orders').find({ id: args.orderId.value }).value()));
 }
 
+
 exports.getOrders = function(args, res, next) {
   /**
    * Returns a list of orders.
@@ -66,6 +97,6 @@ exports.getOrders = function(args, res, next) {
    **/
   res.setHeader('Content-Type', 'application/json');
   res.statusCode = 200;
-  res.end(JSON.stringify(db.get('orders').value()));
+  res.end(JSON.stringify(db.get('orders').map((pizza) => { return pizza.id; })));
 }
 
